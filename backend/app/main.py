abaa81a6dbd5b8983f9a308f5c01cdcd46ee1a99
Kushagra Saini <kushagra.saini19@gmail.com>
2026-09-26 01:01:33 +0530

    added CORS and security headers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from sqlalchemy import text
 
@@ -9,6 +10,8 @@ from app.models.user_model import User
 
 from app.api.user_api import router as user_router
 from app.api.auth_api import router as auth_router
+
+from app.core.security_headers import SecurityHeadersMiddleware
 
 @asynccontextmanager
 async def lifespan(app : FastAPI):
@@ -28,6 +31,20 @@ app = FastAPI(
     description="Data Analysis Platform",
     version="1.0.0",
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+app.add_middleware(
+    SecurityHeadersMiddleware
 )
 
 @app.get("/")
